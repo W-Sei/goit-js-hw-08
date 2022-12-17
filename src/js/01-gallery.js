d2240dd8e5d2f1e1f31e71fdf53cc856e8128598
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-17 20:01:28 +0200

    added SimpleLightbox, adjusted layout + task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from '/src/js/gallery-items';
 // Change code below this line
 
 // console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryGen = document.querySelector('.gallery');
+
+const getItemMarkup = ({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+`;
+const getImagesMarkup = images =>
+  images.map(image => getItemMarkup(image)).join('');
+
+galleryGen.innerHTML = getImagesMarkup(galleryItems);
+galleryGen.addEventListener('click', onClickGallery);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+function onClickGallery(evt) {
+  evt.preventDefault();
+}
